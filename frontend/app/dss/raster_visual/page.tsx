da04b90454d19297724786a0284cf27bb6e1191d
Your Name <you@example.com>
2025-03-27 15:18:49 +0530

    navbsr_fix

---
+++ frontend/app/dss/raster_visual/page.tsx
@@ -0,0 +1,14 @@
+'use client'
+const RasterVisual = () => {
+    return (
+        <div>
+            <h1 className="text-3xl font-bold underline">
+                {/* sidebar for selection the rater files 
+                main component for raster files
+                another component for download */}
+                Raster Visual
+            </h1>
+        </div>
+    )
+}
+export default RasterVisual
